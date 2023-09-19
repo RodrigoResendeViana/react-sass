0e542c9384bb3dd8cd62b4c520f3c4ecf4e984a0
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-19 11:21:20 -0300

    usando sass

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <section className="estrutura">
+        <article className='box'>
+          <h1>Nossa estrutura</h1>
+          <img src="http://lorempixel.com.br/350/200/?1" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <a href="#">
+            Veja mais
+          </a>
+        </article>
+
+        <article className='box'>
+          <h1>Nossa estrutura</h1>
+          <img src="http://lorempixel.com.br/350/200/?2" alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <a href="#">
+            Veja mais
+          </a>
+        </article>
+      </section>
+
+      <section className="estrutura-card">
+        <h1>Criando Mixins</h1>
+
+        <div>
+          <article>
+            <p>
+              lorem
+            </p>
+          </article>
+
+          <article>
+            <p>
+              lorem
+            </p>
+          </article>
+
+          <article>
+            <p>
+              lorem
+            </p>
+          </article>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default App
